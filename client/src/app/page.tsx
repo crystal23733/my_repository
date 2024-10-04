7f4228dfd9723a85426eaa9c30ed16aa9b53d083
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-05 00:37:40 +0900

    :art:업로드 폼 컴포넌트 할당

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,11 +2,33 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import FileUpload from "./components/FileUpload";
+import {useState} from "react";
 
+/**
+ * Home 컴포넌트
+ *
+ * @returns {JSX.Element} - 홈 페이지 컴포넌트
+ */
 export default function Home() {
+  const [files, setFiles] = useState<{ id: number; filename: string }[]>([]);
+
+  /**
+   * 파일 업로드 핸들러
+   * @param {string} title - 업로드할 폴더의 제목
+   * @param {File[]} uploadedFiles - 업로드할 파일 목록
+   */
+  const handleFileUpload = (title: string, uploadedFiles: File[]) => {
+    // 파일 업로드 로직 (API 호출 대신 여기서 임시로 처리)
+    const newFiles = uploadedFiles.map((file) => ({
+      id: Date.now(),
+      filename: file.name,
+    })); // 파일 아이디와 제목 저장 (임시)
+    setFiles((prevFiles) => [...prevFiles, ...newFiles]); // 파일 리스트 업데이트
+    console.log(files);
+  };
   return (
-	  <div>
-	  	<FileUpload />
-	  </div>
-	 );
+    <div>
+      <FileUpload onFileUpload={handleFileUpload} />
+    </div>
+  );
 }
