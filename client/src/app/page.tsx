d7384dc87ea56696f751db6d625a3f42aaf28140
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-06 23:47:19 +0900

    :package:홈화면 모달 컴포넌트 대대적 변경

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,41 +1,32 @@
 "use client";
 
-import React, { useState } from "react";
-import FileUpload from "./components/FileUpload";
-import FileList from "./components/FileList";
+import React from "react";
+import CreatePostForm from "../components/CreatePost/CreatePostForm"; // 게시글 작성 폼 가져오기
+import useModal from "../hook/useModal";
 
 /**
- * 홈 페이지 컴포넌트
+ * 메인 페이지 컴포넌트
  *
- * @returns {JSX.Element} 홈 페이지 JSX 요소
+ * 홈 페이지에서는 게시글 리스트와 글쓰기 버튼을 포함합니다.
+ *
+ * @component
+ *
+ * @returns {JSX.Element} 홈 페이지
  */
-export default function Home() {
-  const [uploadedFiles, setUploadedFiles] = useState<
-    { id: number; filename: string }[]
-  >([]); // 업로드된 파일 리스트 상태
-
-  /**
-   * 파일 업로드 핸들러
-   * @param {string} title - 업로드할 폴더의 제목
-   * @param {File[]} files - 업로드할 파일 목록
-   */
-  const handleFileUpload = (title: string, files: File[]) => {
-    console.log("파일 업로드 핸들러 호출:", { title, files }); // 업로드 핸들러 호출 로그
-
-    const newFiles = files.map((file) => ({
-      id: Date.now() + Math.random(), // 고유한 ID 생성
-      filename: file.webkitRelativePath, // 파일의 상대 경로를 저장
-    }));
-    setUploadedFiles((prevFiles) => [...prevFiles, ...newFiles]); // 파일 리스트 업데이트
-  };
+const HomePage: React.FC = () => {
+  const { isActive, handleModal, closeModal } = useModal();
 
   return (
-    <div>
-      <h1>나의 소스코드 저장소</h1>
-      <FileUpload onFileUpload={handleFileUpload} />{" "}
-      {/* 파일 업로드 컴포넌트 사용 */}
-      <FileList files={uploadedFiles} />{" "}
-      {/* 업로드된 파일 목록 컴포넌트 사용 */}
+    <div className="container">
+      <h1 className="title">내 블로그</h1>
+      <button className="button is-primary" onClick={handleModal}>
+        글쓰기
+      </button>
+
+      {/* 모달 컴포넌트 */}
+      <CreatePostForm isActive={isActive} closeModal={closeModal} />
     </div>
   );
-}
+};
+
+export default HomePage;
