91ed9c4f2f15924fee5579b6a0f19f720421494b
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-08 14:25:28 +0900

    :package:배포 파일 working-directory 설정 변경

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,  // GitHub Pages 배포를 위해 필요
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',  // 레포지토리 이름으로 수정
+  basePath: process.env.NODE_ENV === 'production' ? '/my_repository' : '',  // 레포지토리 이름으로 수정
 };
 
 export default nextConfig;
