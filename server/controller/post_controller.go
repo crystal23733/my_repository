006c45e03ff32a8fc6813632a3c68700a6321245
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-13 00:25:15 +0900

    :package:MongoDB 클라이언트 post연결

--- server/controller/post_controller.go
+++ server/controller/post_controller.go
@@ -10,14 +10,17 @@ import (
 	"server/model"
 
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // PostController는 게시글 관련 요청을 처리하는 컨트롤러이다.
-type PostController struct{}
+type PostController struct{
+	DB *mongo.Client
+}
 
 // NewPostController는 새로운 PostController 인스턴스를 생성한다.
-func NewPostController() *PostController {
-	return &PostController{}
+func NewPostController(db *mongo.Client) *PostController {
+	return &PostController{DB: db}
 }
 
 // Hello World를 출력하는 함수
