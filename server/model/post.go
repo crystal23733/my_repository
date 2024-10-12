a8cfcaf5ba7c49c97f9eec962583a775dc4b5b43
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-13 01:43:25 +0900

    :package:post구조체 변경
    bson태그를 추가하여 MongoDB와의 필드 매핑을 명확히 함.

--- server/model/post.go
+++ server/model/post.go
@@ -2,8 +2,8 @@ package model
 
 // post구조체는 게시글의 구조를 정의한다.
 type Post struct {
-	Title string `json:"title"` // 게시글 제목
-	Content string `json:"content"` // 게시글 내용
-	Images []string `json:"images"` // 이미지 URL 목록
+	Title string `json:"title" bson:"title"` // 게시글 제목
+	Content string `json:"content" bson:"content"` // 게시글 내용
+	Images []string `json:"images" bson:"images"` // 이미지 URL 목록
 }
 
