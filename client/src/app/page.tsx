37bab643972db02723a57bcbb18652e21f2204e9
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-13 13:35:37 +0900

    :art:게시글 리스트 컴포넌트 할당
    더미데이터 생성

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React from "react";
+import React, {useState} from "react";
 import CreatePostForm from "../components/CreatePost/CreatePostForm"; // 게시글 작성 폼 가져오기
+import PostList from "../components/list/PostList";
 import useModal from "../hook/useModal";
 
 /**
@@ -15,6 +16,13 @@ import useModal from "../hook/useModal";
  */
 const HomePage: React.FC = () => {
   const { isActive, handleModal, closeModal } = useModal();
+  
+  // 더미데이터
+  const [posts, setPosts] = useState<{id:string; title:string}[]>([
+	  { id: "1", title: "첫 번째 게시글" },
+    	  { id: "2", title: "두 번째 게시글" },
+	  { id: "3", title: "세 번째 게시글" },
+  ]);
 
   return (
     <div className="container">
@@ -26,6 +34,9 @@ const HomePage: React.FC = () => {
 
       	{/* 모달 컴포넌트 */}
       	<CreatePostForm isActive={isActive} closeModal={closeModal} />
+      </div>
+      <div className="list-box">
+      	<PostList posts={posts}/>
       </div>
     </div>
   );
