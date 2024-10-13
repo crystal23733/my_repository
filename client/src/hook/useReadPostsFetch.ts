ccab43be10b6d8d2b943041aefac0672ee56be07
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-14 00:31:16 +0900

    :sparkles:게시글 상세조회 커스텀 훅
    게시글의 상세 데이터를 요청하는 커스텀 훅

--- client/src/hook/useReadPostsFetch.ts
+++ client/src/hook/useReadPostsFetch.ts
@@ -1,1 +1,43 @@
+import { useState, useEffect } from "react";
+import FetchApi from "../api/lib/FetchApi";
 
+interface ReadDetail {
+  id: string;
+  title: string;
+  content: string;
+  images: string[];
+}
+
+interface UseReadPostsDetailReturn {
+  readDetail: ReadDetail | null;
+  error: string | null;
+  loading: boolean;
+}
+
+export default (postId: string): UseReadPostsDetailReturn => {
+  const [readDetail, setReadDetail] = useState<ReadDetail | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL as string;
+      const DETAIL_URL = process.env.NEXT_PUBLIC_DETAIL_URL as string;
+      const fetchApi = new FetchApi<ReadDetail>(BASE_URL);
+      try {
+        const endpoint = `${DETAIL_URL}/${postId}`;
+        const data = await fetchApi.request(endpoint, "GET");
+        console.log(data);
+        setReadDetail(data);
+      } catch (err) {
+        setError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (postId) {
+      fetchData();
+    }
+  }, [postId]);
+  return { readDetail, error, loading };
+};
